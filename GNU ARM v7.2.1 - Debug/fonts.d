687394ebf7d764115ca0831d6e08adac41e92213
vbirjuko <89239270+vbirjuko@users.noreply.github.com>
2022-07-26 06:54:03 +0300

    delete

--- GNU ARM v7.2.1 - Debug/fonts.d
+++
@@ -1,7 +0,0 @@
-fonts.o: ../fonts.c \
- c:\users\wl\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- ../fonts.h
-
-c:\users\wl\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-
-../fonts.h:
--- GNU ARM v7.2.1 - Debug/gecko_sdk_4.0.2/platform/service/system/src/sl_system_process_action.d
+++
@@ -1,8 +0,0 @@
-gecko_sdk_4.0.2/platform/service/system/src/sl_system_process_action.o: \
- C:/Users/wl/SimplicityStudio/SDKs/gecko_sdk/platform/service/system/src/sl_system_process_action.c \
- c:\users\wl\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- C:\Users\wl\SimplicityStudio\v5_workspace\Maze\autogen/sl_event_handler.h
-
-c:\users\wl\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-
-C:\Users\wl\SimplicityStudio\v5_workspace\Maze\autogen/sl_event_handler.h:
